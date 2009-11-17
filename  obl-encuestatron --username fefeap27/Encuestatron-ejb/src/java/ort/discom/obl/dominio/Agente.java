23b967d746bd8cdae804a95cfd4a8a2f9b9c8996
estarias <estarias@97e34a2a-d163-11de-a306-d16691e8d855>
2009-11-17 02:24:39 +0000

    Se agregaron entidades. Faltaría resolver como son las relaciones uno a muchos. acutalmente lo armé con arraylist, pero no sé como sería para que quede enganchado con la base de datos.

---  obl-encuestatron --username fefeap27/Encuestatron-ejb/src/java/ort/discom/obl/dominio/Agente.java
+++  obl-encuestatron --username fefeap27/Encuestatron-ejb/src/java/ort/discom/obl/dominio/Agente.java
@@ -10,11 +10,9 @@ import javax.persistence.Id;
 
 
 @Entity
-public class Agente implements Serializable {
+public class Agente extends Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    
     private ArrayList losClientes;
 
     public ArrayList getLosClientes() {
@@ -23,39 +21,6 @@ public class Agente implements Serializable {
 
     public void setLosClientes(ArrayList losClientes) {
         this.losClientes = losClientes;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Agente)) {
-            return false;
-        }
-        Agente other = (Agente) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "ort.discom.obl.dominio.Agente[id=" + id + "]";
-    }
+    } 
 
 }
---  obl-encuestatron --username fefeap27/Encuestatron-ejb/src/java/ort/discom/obl/dominio/Encuesta.java
+++  obl-encuestatron --username fefeap27/Encuestatron-ejb/src/java/ort/discom/obl/dominio/Encuesta.java
@@ -1,14 +1,67 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ort.discom.obl.dominio;
 
-/**
- *
- * @author Felipe
- */
-public class Encuesta {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+
+@Entity
+public class Encuesta implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private Cliente propietario;
+    private ArrayList lasPreguntas = new ArrayList();
+    private String nombre;
+    private String clave;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha_ingreso;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha_modificacion;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha_comienzo;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fecha_cierre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Encuesta)) {
+            return false;
+        }
+        Encuesta other = (Encuesta) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ort.discom.obl.dominio.Encuesta[id=" + id + "]";
+    }
 
 }
